a6cbca87a1db1e3edac97a3d967dacd191b44b12
Ashley Davis <ashley.davis@realseriousgames.com>
2015-11-02 15:02:08 +1000

    Experimenting with possible API.

---
+++ src/IColumn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pancas
+{
+    public interface IColumn<T>
+    {
+        string Name { get; }
+
+        IEnumerable<T> Values();
+    }
+}
---
+++ src/IDataFrame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pancas
+{
+    public interface IDataFrame
+    {
+        IEnumerable<Tuple<T1>> Rows<T1>();
+        IEnumerable<Tuple<T1, T2>> Rows<T1, T2>();
+        IEnumerable<Tuple<T1, T2, T3>> Rows<T1, T2, T3>();
+
+        IEnumerable<string> Columns();
+
+        IColumn<T> Column<T>(string columnName);
+        IColumn<T> Column<T>(int columnIndex);
+    }
+}
