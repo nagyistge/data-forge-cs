23e27b8f76f4ec47507c708317caf2f3b2ee4139
Ashley Davis <ashley@codecapers.com.au>
2015-11-27 17:37:31 +1000

    Merged IDataFrame into the same file as the class.

--- src/DataFrame.cs
+++ src/DataFrame.cs
@@ -6,6 +6,39 @@ using System.Text;
 
 namespace Pancas
 {
+    public interface IDataFrame : IEnumerable<IRow>
+    {
+        //todo: are these even needed?
+        IEnumerable<Tuple<T1>> GetValues<T1>();
+        IEnumerable<Tuple<T1, T2>> GetValues<T1, T2>();
+        IEnumerable<Tuple<T1, T2, T3>> GetValues<T1, T2, T3>();
+
+        /// <summary>
+        /// Get names of the columns in the data frame.
+        /// </summary>
+        IEnumerable<string> GetColumnNames();
+
+        IColumn GetColumn(string columnName);
+        IColumn GetColumn(int columnIndex);
+
+        IDataFrameSerializer As(IDataFormatPlugin dataFormatPlugin);
+
+        IEnumerable<IColumn> GetColumns();
+
+        IDataFrame DropColumn(string columnName);
+        IDataFrame GetColumnsSubset(IEnumerable<string> columnNames);
+
+        IDataFrame Where(Func<IRow, bool> rowPredicate);
+
+        IDataFrame SetColumn<T>(string columnName, IEnumerable<T> data);
+        IDataFrame SetColumn(string columnName, IColumn column);
+
+        /// <summary>
+        /// Convert index of the specified column.
+        /// </summary>
+        int GetColumnIndex(string columName);
+    }
+
     public class DataFrame : IDataFrame
     {
         private IColumn[] columns;
